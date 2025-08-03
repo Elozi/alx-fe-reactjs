b3dac3743292a2312c608f81d8d77e1fd31ed575
Elozi <Abbyjamezzz@gmail.com>
2025-08-03 22:16:05 +0100

    fixed error

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -36,7 +36,7 @@ function Search() {
       </form>
 
       {loading && <p>Loading...</p>}
-      {error && <p>Looks like we can't find the user.</p>}
+      {error && <p>Looks like we cant find the user.</p>}
       {user && (
         <div style={{ marginTop: '1rem' }}>
           <img src={user.avatar_url} alt="Avatar" width={100} />
